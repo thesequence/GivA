fdf749246e9cf327d0be0cc5f36f0a0a02e9b98c
Eunjeong Park <ej.meyerpark@googlemail.com>
2022-08-30 17:24:22 +0200

    reviews cotroller needs show template

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-    resources :chats, only: :show do
-      resources :messages, only: :create
-    end
+  resources :users, only: :show do
+    resources :reviews, only: [:new, :create]
+  end
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :chats, only: :show do
+    resources :messages, only: :create
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end

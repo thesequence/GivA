4d8324e2a9cb8483f52c2010dd1669c3f5833e83
Eunjeong Park <ej.meyerpark@googlemail.com>
2022-09-01 17:43:25 +0200

    MIGRATION CHANGE, REVIEW AND BUDDY CONTROLLERS

--- app/models/buddy.rb
+++ app/models/buddy.rb
@@ -1,4 +1,7 @@
 class Buddy < ApplicationRecord
   belongs_to :asker, class_name: "User"
   belongs_to :receiver, class_name: "User"
+  validates_uniqueness_of :asker, scope: [:receiver]
+
+  enum status: [:accepted, :rejected, :pending]
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,22 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :profiles, only: %i[show index] do
+
+  resources :buddies, only: [:index, :show] do
+    resources :messages, only: :create
     resources :reviews, only: [:new, :create]
   end
+
+  resources :profiles, only: %i[show index] do
+    resources :buddies, only: [:new, :create]
+  end
+
   root to: "pages#home"
 
   # resources :users, only: :show do
    # resources :reviews, only: [:new, :create]
   # end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :chats, only: :show do
-    resources :messages, only: :create
-  end
+
   # Defines the root path route ("/")
   # root "articles#index"
 end

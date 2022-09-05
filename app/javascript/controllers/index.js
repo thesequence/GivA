097db9882b867128906ec0d96d4d92244093850e
Eunjeong Park <ej.meyerpark@googlemail.com>
2022-09-05 12:04:54 +0200

    finish landingPage with rotate

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -7,5 +7,8 @@ import { application } from "./application"
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
+import LogoRotateController from "./logo_rotate_controller"
+application.register("logo-rotate", LogoRotateController)
+
 import ReadmoreController from "./readmore_controller"
 application.register("readmore", ReadmoreController)

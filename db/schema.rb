f516383e3e934753821f3d1ac75ef4dbbd9f135a
Eunjeong Park <ej.meyerpark@googlemail.com>
2022-09-05 15:42:13 +0200

    map

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_02_140206) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_05_103738) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -60,6 +60,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_02_140206) do
     t.time "closing_hour"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "messages", force: :cascade do |t|

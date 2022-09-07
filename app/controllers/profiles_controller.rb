2e3248340384759b577908f82887cf8b26a5d23a
Eunjeong Park <ej.meyerpark@googlemail.com>
2022-09-07 12:51:22 +0200

    finish search tags

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,6 +2,13 @@ class ProfilesController < ApplicationController
   def index
     sleep 3
     @profiles = User.all
+    if params[:query].present?
+      @profiles = User.joins(:tags).where("tags.title ILIKE ?", "%#{params[:query]}%")
+      # @tags = Tag.where("address LIKE ?", "%#{params[:query]}%")
+    else
+      # @tags = Tag.all
+      @profiles = User.all
+    end
   end
 
   def show
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_06_145310) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_07_101040) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -82,6 +82,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_06_145310) do
     t.datetime "updated_at", null: false
     t.index ["chat_id"], name: "index_messages_on_chat_id"
     t.index ["sender_id"], name: "index_messages_on_sender_id"
+  end
+
+  create_table "pg_search_documents", force: :cascade do |t|
+    t.text "content"
+    t.string "searchable_type"
+    t.bigint "searchable_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable"
   end
 
   create_table "reviews", force: :cascade do |t|

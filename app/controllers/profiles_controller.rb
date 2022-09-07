eac9e2d3e18ba5339643ef5a0d1a49339deaebca
Denis Makarov <qamakarov@gmail.com>
2022-09-07 14:01:03 +0200

    add buddies

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,6 +2,13 @@ class ProfilesController < ApplicationController
   def index
     sleep 3
     @profiles = User.all
+    if current_user.displaced
+      @profiles = User.where(displaced: false)
+    elsif current_user.displaced == false
+      @profiles = User.where(displaced: true)
+    else
+      @profiles = User.all
+    end
   end
 
   def show

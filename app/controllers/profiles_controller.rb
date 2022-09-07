14548a17fdd94b19a3509c0ffd2b7382e500cac9
Eunjeong Park <ej.meyerpark@googlemail.com>
2022-09-07 13:00:08 +0200

    profiles

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,9 +4,7 @@ class ProfilesController < ApplicationController
     @profiles = User.all
     if params[:query].present?
       @profiles = User.joins(:tags).where("tags.title ILIKE ?", "%#{params[:query]}%")
-      # @tags = Tag.where("address LIKE ?", "%#{params[:query]}%")
     else
-      # @tags = Tag.all
       @profiles = User.all
     end
   end
